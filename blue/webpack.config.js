e518137c2e0d4a051d8e4ed0e1e55cd6c632919d
MeastBlue <amalou.massinissa@gmail.com>
2023-06-06 22:52:51 +0200

    add blue

---
+++ blue/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "ui-lib",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: ["style-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts"],
+  },
+  externals: {
+    react: "react",
+  },
+};
